50cf69ed0fd025fa5af8fcdc425c7766b7f2d886
LY GIA LONG <48001678+gialong98@users.noreply.github.com>
2019-05-11 16:47:26 +0700

    Update user.js

--- src/routers/user.js
+++ src/routers/user.js
@@ -12,10 +12,9 @@ router.post('/users', async(req,res) => {
     try {
         await user.save()
         sendWelcomeEmail(user.email, user.name)
-        // const token = await user.createAuthToken() --- Khi nào muốn đăng kí xong check token vào trang chủ luôn thì tạo token 
-        res.status(201).send({user}) // status 201 là create done
+        res.status(201).send({user}) // status 201 create done
     } catch(err) {
-        res.status(400).send(err) //status 400 là bad request
+        res.status(400).send(err) //status 400  bad request
     }
 })
 
@@ -49,10 +48,9 @@ router.get('/users/me',auth, async(req,res) => {
 
 
 router.patch('/users/updateMe',auth, async (req,res) => {
-    const updates = Object.keys(req.body) // lấy các đối tượng field trong req.body đc nhập
-    const allowedUpdates = ['name','age','email','password'] // các field cho phép thay đổi value
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update)) // updates dùng every duyệt tất cả phần tử key field
-    // gán vào biến update rồi includes vào allowedUpdates để kiểm tra trùng với các field cho phép
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name','age','email','password'] 
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
     if(!isValidOperation){
         return res.status(400).send({"Error" : "Field inValidOperation"})
@@ -61,14 +59,11 @@ router.patch('/users/updateMe',auth, async (req,res) => {
     const data = req.body
     try {
         const user = await User.findById(req.user._id)
-
-        // hàm này có nghĩa là dùng updates ở trên lấy các field thay đổi
-        // sau đó gán user vừa tìm đc với tên field mà forEach updates tìm đc khi ng dùng nhập vào
-        // gán bằng với các values nhập trong field (req.body) xứng với từng feild
+        
         updates.forEach((update) => {
             user[update] = data[update]
         })
-        // const user = await User.findByIdAndUpdate(_id,data, { new : true, runValidators : true})
+       
         await user.save()
         if(!user){
             res.status(404).send()
@@ -81,10 +76,6 @@ router.patch('/users/updateMe',auth, async (req,res) => {
 
 router.delete('/users/deleteMe', auth, async(req,res) => {
     try {
-        // const user = await User.findByIdAndRemove(req.user._id)
-        // if(!user) {
-        //     return res.status(404).send({Error : "User Not Found"})
-        // }
         await req.user.remove()
         sendRemoveEmail(req.user.email, req.user.name)
         res.send(req.user)
@@ -97,7 +88,7 @@ router.post('/users/logout', auth, async(req,res) => {
     res.send(req.user.tokens)
     try {
         req.user.tokens = req.user.tokens.filter((tokens) => {
-            return tokens.token !== req.token // trả về array token k có req.token , giống như xóa đi vậy đó
+            return tokens.token !== req.token 
         })
         await req.user.save()
         res.send()
@@ -119,7 +110,7 @@ router.post('/users/logoutAll',auth, async(req,res) => {
 
 const multer = require('multer')
 const upload = multer({
-    // dest : 'images', // khi để dest thì middleware này sẽ lấy dữ liệu bỏ vào , nếu k sẽ pass dữ liệu vào function để sử lí
+   
     limits : {
         fileSize : 1000000 // 1 triệu byte ~ 1mb
     },
@@ -131,12 +122,10 @@ const upload = multer({
     }
 })
 
-// upload avatar của user
+// upload avatar user
 router.post('/users/me/avatar', auth, upload.single('avatar'), async(req,res) => {
     const buffer = await sharp(req.file.buffer).resize({ width : 250, height : 250}).png().toBuffer()
-    // bỏ dữ liệu buffer vào sharp. Resize ảnh, convert ảnh thành png , sau đó trả về dữ liệu buffer rồi gán vào biến
-
-    req.user.avatar = buffer // req.file.buffer sẽ giữ dữ liệu image pass từ middle và gán vào user avatar
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 },(err,req,res,next)=> {
